46b56f0505ac2aeb0421d653e537144febd10ba2
Sehwan Kim <skim451@illinois.edu>
2018-01-25 11:53:28 +0900

    [Added] login feature

--- src/main/java/com/woowahan/codesquad001/UserController.java
+++ src/main/java/com/woowahan/codesquad001/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 @RequestMapping("/users")
 public class UserController {
@@ -26,5 +28,23 @@ public class UserController {
         User user = userRepository.findOne(id);
         model.addAttribute("user", user);
         return "user/list";
+    }
+
+    @PostMapping("/login")
+    public String login(String userId, String password, HttpSession session) {
+        User user = userRepository.findByUserId(userId);
+
+        if (user == null)
+            return "user/login_failed";
+
+        if (user.getPassword() == null)
+            return "user/login_failed";
+
+        if (user.getPassword().equals(password)) {
+            session.setAttribute("sessionedUser", user);
+            return "redirect:/";
+        }
+
+        return "user/login_failed";
     }
 }
