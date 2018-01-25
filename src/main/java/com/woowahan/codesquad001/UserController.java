1b9bed5601d78957a825c26e8974ab3deb8d7245
Sehwan Kim <skim451@illinois.edu>
2018-01-25 18:24:41 +0900

    Added user list page

--- src/main/java/com/woowahan/codesquad001/UserController.java
+++ src/main/java/com/woowahan/codesquad001/UserController.java
@@ -9,6 +9,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Controller
 @RequestMapping("/users")
@@ -21,6 +25,13 @@ public class UserController {
     public String create(User user) {
         userRepository.save(user);
         return "redirect:/";
+    }
+
+    @GetMapping("")
+    public String showList(Model model) {
+        Iterable<User> userList = userRepository.findAll();
+        model.addAttribute("userList", userList);
+        return "user/list";
     }
 
     @GetMapping("/{id}")
